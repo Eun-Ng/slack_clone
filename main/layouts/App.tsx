2b8ac741c0811e4d306c0f2e5961115996f4312c
Eun-Ng <dhkdldms@gmail.com>
2022-09-22 23:44:50 +0900

    Add: dir structure, react router

--- main/layouts/App.tsx
+++ main/layouts/App.tsx
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/Login';
+import SignUp from '@pages/SignUp';
 
 const App = () => {
-  return <div>hi there</div>;
+  return (
+    <Switch>
+      <Redirect exact path="/" to="/login" />
+      <Route path="/login" component={LogIn} />
+      <Route path="/signup" component={SignUp} />
+    </Switch>
+  );
 };
 
 export default App;

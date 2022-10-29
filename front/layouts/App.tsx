69315faeeef3a5c9c25cf74975b808ad54d94365
Eun-Ng <dhkdldms@gmail.com>
2022-10-29 23:35:55 +0900

    Add: Front initial settings
    webpack, package.json, tsconfig, eslint, prettier settings

---
+++ front/layouts/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import LogIn from '@pages/LogIn';
+import SignUp from '@pages/SignUp';
+
+const App = () => {
+  return <div>핫 리로딩</div>;
+};
+
+export default App;

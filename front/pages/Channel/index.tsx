adadf56357daee0164f67d7f305d25b51b14d9ae
Eun-Ng <dhkdldms@gmail.com>
2022-11-10 22:16:31 +0900

    feat: add channel component

---
+++ front/pages/Channel/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Workspace from '@layouts/Workspace';
+
+const Channel = () => {
+  return (
+    <Workspace>
+      <div>LogIn Success</div>
+    </Workspace>
+  );
+};
+
+export default Channel;

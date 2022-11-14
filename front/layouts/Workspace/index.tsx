cc636e2d5626ebaecd4548ccd0a23705052b1700
Eun-Ng <dhkdldms@gmail.com>
2022-11-14 23:54:30 +0900

    feat: add workspace

--- front/layouts/Workspace/index.tsx
+++ front/layouts/Workspace/index.tsx
@@ -9,14 +9,20 @@ import {
   WorkspaceName,
   MenuScroll,
 } from '@layouts/Workspace/styles';
+import loadable from '@loadable/component';
 import fetcher from '@utils/fetcher';
 import axios from 'axios';
-import React, { FC, useCallback } from 'react';
-import { Redirect } from 'react-router-dom';
+import React, { FC, useState, useCallback } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import gravatar from 'gravatar';
 import useSWR from 'swr';
+import Menu from '@components/Menu';
 
-const Workspace: FC = ({ children }) => {
+const Channel = loadable(() => import('@pages/Channel'));
+const DirectMessage = loadable(() => import('@pages/DirectMessage'));
+
+const Workspace: FC = () => {
+  const [showUserMenu, setShowUserMenu] = useState(false);
   const { data, error, mutate } = useSWR('http://localhost:3095/api/users', fetcher);
 
   const onLogout = useCallback(() => {
@@ -29,6 +35,10 @@ const Workspace: FC = ({ children }) => {
       });
   }, []);
 
+  const onClickUserProfile = useCallback(() => {
+    setShowUserMenu((prev) => !prev);
+  }, []);
+
   if (!data) {
     return <Redirect to={'/login'} />;
   }
@@ -37,7 +47,14 @@ const Workspace: FC = ({ children }) => {
     <div>
       <Header>
         <RightMenu>
-          <ProfileImg src={gravatar.url(data.email, { s: '20px', d: 'retro' })} alt={data.nickname} />
+          <span onClick={onClickUserProfile}>
+            <ProfileImg src={gravatar.url(data.email, { s: '20px', d: 'retro' })} alt={data.nickname} />
+            {showUserMenu && (
+              <Menu style={{ right: 0, top: 38 }} show={showUserMenu} onClickModal={onClickUserProfile}>
+                프로필메뉴
+              </Menu>
+            )}
+          </span>
         </RightMenu>
       </Header>
       <button onClick={onLogout}>로그아웃</button>
@@ -47,9 +64,13 @@ const Workspace: FC = ({ children }) => {
           <WorkspaceName>Slack Clone</WorkspaceName>
           <MenuScroll>menu scroll</MenuScroll>
         </Channels>
-        <Chats>chats</Chats>
+        <Chats>
+          <Switch>
+            <Route path="/workspace/channel" component={Channel} />
+            <Route path="/workspace/dm" component={DirectMessage} />
+          </Switch>
+        </Chats>
       </WorkspaceWrapper>
-      {children}
     </div>
   );
 };

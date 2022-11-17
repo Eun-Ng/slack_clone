b745e326c767eb44b239ab9a7717b8ab3eb8d3fe
Eun-Ng <dhkdldms@gmail.com>
2022-11-18 03:29:11 +0900

    Add: InviteChannelModal component

---
+++ front/components/InviteChannelModal/index.tsx
@@ -0,0 +1,60 @@
+import React, { FC, useCallback } from 'react';
+import { useParams } from 'react-router';
+import useInput from '@hooks/useInput';
+import useSWR from 'swr';
+import { IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import Modal from '@components/Modal';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowInviteChannelModal: (flag: boolean) => void;
+}
+
+const InviteChannelModal: FC<Props> = ({ show, onCloseModal, setShowInviteChannelModal }) => {
+  const { workspace, channel } = useParams<{ workspace: string; channel: string }>();
+  const [newMember, onChangeNewMember, setNewMember] = useInput('');
+  const { data: userData } = useSWR<IUser>('/api/users', fetcher);
+  const { mutate: mutateMembers } = useSWR<IUser[]>(
+    userData && channel ? `/api/workspaces/${workspace}/channels/${channel}/members` : null,
+    fetcher,
+  );
+
+  const onInviteMember = useCallback(
+    (e) => {
+      e.preventDefault();
+      axios
+        .post(`/api/workspaces/${workspace}/channels/${channel}/members`, {
+          email: newMember,
+        })
+        .then(() => {
+          mutateMembers();
+          setShowInviteChannelModal(false);
+          setNewMember('');
+        })
+        .catch((error) => {
+          console.dir(error);
+          toast.error(error.response?.data, { position: 'bottom-center' });
+        });
+    },
+    [newMember],
+  );
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onInviteMember}>
+        <Label id="member-label">
+          <span>채널 멤버 초대</span>
+          <Input id="member" type="email" value={newMember} onChange={onChangeNewMember} />
+        </Label>
+        <Button type="submit">초대하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default InviteChannelModal;

229ee27d9cc73b5daaca8768cef8b0fc13e3f761
Eun-Ng <dhkdldms@gmail.com>
2022-11-18 03:29:02 +0900

    Add: InviteWorkspaceModal component

---
+++ front/components/InviteWorkspaceModal/index.tsx
@@ -0,0 +1,59 @@
+import React, { FC, useCallback } from 'react';
+import { useParams } from 'react-router';
+import { Button, Input, Label } from '@pages/SignUp/styles';
+import useInput from '@hooks/useInput';
+import useSWR from 'swr';
+import { IChannel, IUser } from '@typings/db';
+import fetcher from '@utils/fetcher';
+import Modal from '@components/Modal';
+import axios from 'axios';
+import { toast } from 'react-toastify';
+
+interface Props {
+  show: boolean;
+  onCloseModal: () => void;
+  setShowInviteWorkspaceModal: (flag: boolean) => void;
+}
+
+const InviteWorkspaceModal: FC<Props> = ({ show, onCloseModal, setShowInviteWorkspaceModal }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
+  const [newMember, onChangeNewMember, setNewMember] = useInput('');
+  const { data: userData } = useSWR<IUser>('/api/users', fetcher);
+  const { mutate: mutateMember } = useSWR<IChannel[]>(
+    userData ? `/api/workspaces/${workspace}/members` : null,
+    fetcher,
+  );
+
+  const onInviteMember = useCallback((e) => {
+    e.preventDefault();
+    if (!newMember || !newMember.trim()) return; // 인풋창 검사
+
+    axios
+      .post(`/api/workspaces/${workspace}/members`, {
+        email: newMember,
+      })
+      .then((response) => {
+        mutateMember(response.data, false);
+        setShowInviteWorkspaceModal(false);
+        setNewMember('');
+      })
+      .catch((error) => {
+        console.dir(error);
+        toast.error(error.response?.data, { position: 'bottom-center' });
+      });
+  }, []);
+
+  return (
+    <Modal show={show} onCloseModal={onCloseModal}>
+      <form onSubmit={onInviteMember}>
+        <Label id="member-label">
+          <span>이메일</span>
+          <Input id="member" type="email" value={newMember} onChange={onChangeNewMember} />
+        </Label>
+        <Button type="submit">초대하기</Button>
+      </form>
+    </Modal>
+  );
+};
+
+export default InviteWorkspaceModal;

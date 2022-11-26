5eb57e25535d89dcafbbbb992c8cbd91588b1581
Eun-Ng <dhkdldms@gmail.com>
2022-11-26 22:37:17 +0900

    Add: DM components

--- front/pages/DirectMessage/index.tsx
+++ front/pages/DirectMessage/index.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Container, Header } from '@pages/DirectMessage/styles';
+import gravatar from 'gravatar';
+import useSWR from 'swr';
+import fetcher from '@utils/fetcher';
 
 const DirectMessage = () => {
-  return <div>dm!</div>;
+  const { data: userData } = useSWR(`/api/workspaces/${workspace}/members/${id}`, fetcher);
+
+  return (
+    <Container>
+      <Header>
+        <img src={gravatar.url(userData.email, { s: '24px', d: 'retro' })} alt={userData.nickname} />
+        <span>{userData.nickname}</span>
+      </Header>
+    </Container>
+  );
 };
 
 export default DirectMessage;

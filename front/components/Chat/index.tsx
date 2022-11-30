67310c49d32f61af3e368a31c588112d1c31b060
Eun-Ng <dhkdldms@gmail.com>
2022-11-30 23:55:44 +0900

    Add: Chat components

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -2,13 +2,36 @@ import { IDM } from '@typings/db';
 import React, { VFC } from 'react';
 import { ChatWrapper } from '@components/Chat/styles';
 import gravatar from 'gravatar';
+import dayjs from 'dayjs';
+import regexifyString from 'regexify-string';
+import { Link } from 'react-router-dom';
+import { useParams } from 'react-router';
+import workspace from '@layouts/Workspace';
+import channel from '@pages/Channel';
 
 interface Props {
   data: IDM;
 }
 
 const Chat: VFC<Props> = ({ data }) => {
+  const { workspace } = useParams<{ workspace: string; channel: string }>();
   const user = data.Sender;
+
+  const result = regexifyString({
+    pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
+    decorator(match, index) {
+      const arr = match.match(/@\[(.+?)]\((\d+?)\)/)!;
+      if (arr) {
+        return (
+          <Link key={match + index} to={`/workspace/${workspace}/dm/${arr[2]}`}>
+            @{arr[1]}
+          </Link>
+        );
+      }
+      return <br key={index} />;
+    },
+    input: data.content,
+  });
 
   return (
     <ChatWrapper>
@@ -18,9 +41,9 @@ const Chat: VFC<Props> = ({ data }) => {
       <div className="chat-text">
         <div className="chat-user">
           <b>{user.nickname}</b>
-          <span>{data.createdAt}</span>
+          <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
-        <p>{data.content}</p>
+        <p>{result}</p>
       </div>
     </ChatWrapper>
   );

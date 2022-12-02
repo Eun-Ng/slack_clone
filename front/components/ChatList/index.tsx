ca4568b609cfeae2b2c2d3fe24594eb1380f5a65
Eun-Ng <dhkdldms@gmail.com>
2022-12-02 17:01:26 +0900

    Update: Props type & Scroll options

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,26 +1,50 @@
-import React, { useCallback, useRef, VFC } from 'react';
-import { ChatZone, Section } from '@components/ChatList/styles';
-import { IDM } from '@typings/db';
+import React, { useCallback, forwardRef, MutableRefObject, RefObject } from 'react';
+import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
+import { IChat, IDM } from '@typings/db';
 import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
 interface Props {
-  chatData?: IDM[];
+  chatSections: { [key: string]: (IDM | IChat)[] };
+  setSize: (f: (size: number) => number) => Promise<(IDM | IChat)[][] | undefined>;
+  isReachingEnd: boolean;
 }
 
-const ChatList: VFC<Props> = ({ chatData }) => {
-  const scrollbarRef = useRef(null);
-  const onScroll = useCallback(() => {}, []);
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isReachingEnd }, scrollRef) => {
+  const onScroll = useCallback(
+    (values) => {
+      if (values.scrollTop === 0 && !isReachingEnd) {
+        console.log('가장 위');
+        setSize((prevSize) => prevSize + 1).then(() => {
+          // 스크롤 위치 유지
+          const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
+          if (current) {
+            current.scrollTop(current.getScrollHeight() - values.scrollHeight);
+          }
+        });
+      }
+    },
+    [scrollRef, isReachingEnd, setSize],
+  );
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
-        {chatData?.map((chat) => (
-          <Chat key={chat.id} data={chat} />
-        ))}
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
+        {Object.entries(chatSections).map(([date, chats]) => {
+          return (
+            <Section className={`section-${date}`} key={date}>
+              <StickyHeader>
+                <button>{date}</button>
+              </StickyHeader>
+              {chats.map((chat) => (
+                <Chat key={chat.id} data={chat} />
+              ))}
+            </Section>
+          );
+        })}
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;

ab7adc8734324a35d337efd755e2b86fe172f408
Eun-Ng <dhkdldms@gmail.com>
2022-12-23 10:26:11 +0900

    Revert "fix: refactoring"
    This reverts commit e084e63ba7bc8e32c3327d211f9fa472d0870c0d.

--- front/components/ChatList/index.tsx
+++ front/components/ChatList/index.tsx
@@ -1,31 +1,35 @@
-import Chat from '@components/Chat';
+import React, { useCallback, forwardRef, MutableRefObject, RefObject } from 'react';
 import { ChatZone, Section, StickyHeader } from '@components/ChatList/styles';
 import { IChat, IDM } from '@typings/db';
-import React, { FC, RefObject, useCallback } from 'react';
+import Chat from '@components/Chat';
 import { Scrollbars } from 'react-custom-scrollbars-2';
 
 interface Props {
-  scrollbarRef: RefObject<Scrollbars>;
-  isReachingEnd?: boolean;
-  isEmpty: boolean;
   chatSections: { [key: string]: (IDM | IChat)[] };
   setSize: (f: (size: number) => number) => Promise<(IDM | IChat)[][] | undefined>;
+  isReachingEnd: boolean;
 }
-const ChatList: FC<Props> = ({ scrollbarRef, isReachingEnd, isEmpty, chatSections, setSize }) => {
+
+const ChatList = forwardRef<Scrollbars, Props>(({ chatSections, setSize, isReachingEnd }, scrollRef) => {
   const onScroll = useCallback(
     (values) => {
-      if (values.scrollTop === 0 && !isReachingEnd && !isEmpty) {
-        setSize((size) => size + 1).then(() => {
-          scrollbarRef.current?.scrollTop(scrollbarRef.current?.getScrollHeight() - values.scrollHeight);
+      if (values.scrollTop === 0 && !isReachingEnd) {
+        console.log('가장 위');
+        setSize((prevSize) => prevSize + 1).then(() => {
+          // 스크롤 위치 유지
+          const current = (scrollRef as MutableRefObject<Scrollbars>)?.current;
+          if (current) {
+            current.scrollTop(current.getScrollHeight() - values.scrollHeight);
+          }
         });
       }
     },
-    [setSize, scrollbarRef, isReachingEnd, isEmpty],
+    [scrollRef, isReachingEnd, setSize],
   );
 
   return (
     <ChatZone>
-      <Scrollbars autoHide ref={scrollbarRef} onScrollFrame={onScroll}>
+      <Scrollbars autoHide ref={scrollRef} onScrollFrame={onScroll}>
         {Object.entries(chatSections).map(([date, chats]) => {
           return (
             <Section className={`section-${date}`} key={date}>
@@ -41,6 +45,6 @@ const ChatList: FC<Props> = ({ scrollbarRef, isReachingEnd, isEmpty, chatSection
       </Scrollbars>
     </ChatZone>
   );
-};
+});
 
 export default ChatList;

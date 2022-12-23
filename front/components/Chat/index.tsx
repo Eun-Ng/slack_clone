457e03ed87b1a4efa06a3adb645caa3f3da55d7c
Eun-Ng <dhkdldms@gmail.com>
2022-12-23 10:25:42 +0900

    fix: refactoring

--- front/components/Chat/index.tsx
+++ front/components/Chat/index.tsx
@@ -1,28 +1,27 @@
-import { IChat, IDM } from '@typings/db';
-import React, { memo, useMemo, VFC } from 'react';
 import { ChatWrapper } from '@components/Chat/styles';
-import gravatar from 'gravatar';
+import { IChat, IDM, IUser } from '@typings/db';
 import dayjs from 'dayjs';
+import gravatar from 'gravatar';
+import React, { FC, useMemo, memo } from 'react';
+import { useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 import regexifyString from 'regexify-string';
-import { Link, useParams } from 'react-router-dom';
 
 interface Props {
   data: IDM | IChat;
 }
 
 const BACK_URL = process.env.NODE_ENV === 'development' ? 'http://localhost:3095' : '';
-const Chat: VFC<Props> = memo(({ data }) => {
+const Chat: FC<Props> = memo(({ data }) => {
   const { workspace } = useParams<{ workspace: string; channel: string }>();
-  const user = 'Sender' in data ? data.Sender : data.User;
+  const user: IUser = 'Sender' in data ? data.Sender : data.User;
 
-  const result = useMemo(
+  const result = useMemo<(string | JSX.Element)[] | JSX.Element>(
     () =>
-      // uploads\\서버주소
       data.content.startsWith('uploads\\') || data.content.startsWith('uploads/') ? (
-        <img src={`${BACK_URL}/${data.content}`} style={{ maxHeight: 200, borderRadius: 5 }} />
+        <img src={`${BACK_URL}/${data.content}`} style={{ maxHeight: 200 }} />
       ) : (
         regexifyString({
-          input: data.content,
           pattern: /@\[(.+?)]\((\d+?)\)|\n/g,
           decorator(match, index) {
             const arr: string[] | null = match.match(/@\[(.+?)]\((\d+?)\)/)!;
@@ -35,6 +34,7 @@ const Chat: VFC<Props> = memo(({ data }) => {
             }
             return <br key={index} />;
           },
+          input: data.content,
         })
       ),
     [workspace, data.content],
@@ -43,11 +43,11 @@ const Chat: VFC<Props> = memo(({ data }) => {
   return (
     <ChatWrapper>
       <div className="chat-img">
-        <img src={gravatar.url(user?.email, { s: '36px', d: 'retro' })} alt={user?.nickname} />
+        <img src={gravatar.url(user.email, { s: '36px', d: 'retro' })} alt={user.nickname} />
       </div>
       <div className="chat-text">
         <div className="chat-user">
-          <b>{user?.nickname}</b>
+          <b>{user.nickname}</b>
           <span>{dayjs(data.createdAt).format('h:mm A')}</span>
         </div>
         <p>{result}</p>
